b38fbf3b826836491dd30cddc92eb4d7fbe89144
Muhammad Jazeel Ottupara <muhammadjazeel006@gmail.com>
2024-12-09 21:44:27 -0500

    static root push

--- autoassist/settings.py
+++ autoassist/settings.py
@@ -148,6 +148,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
